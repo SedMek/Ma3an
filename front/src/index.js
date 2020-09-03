12584158489c8b8db7c7416d1685768f6b25cb10
Seddik <s.mekki@kayrros.com>
2020-09-04 01:26:35 +0200

    fix backend enpoint in frontend

--- front/src/index.js
+++ front/src/index.js
@@ -10,8 +10,8 @@ import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import "./App.scss";
 
 import socketIOClient from "socket.io-client";
-// const ENDPOINT = "http://localhost:4001";
-const ENDPOINT = "https://6d707240a653.ngrok.io/";
+const ENDPOINT = "http://localhost:4001";
+// const ENDPOINT = "https://6d707240a653.ngrok.io/";
 
 console.log("initiating socket connection ...");
 const socket = socketIOClient(ENDPOINT);
