0852e115d692d03f605a555648aeddeeb472eb33
Seddik <s.mekki@kayrros.com>
2020-09-03 23:53:10 +0200

    reinit git

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
+const path = require("path");
+
+const app = express();
+app.use(index);
+
+const server = http.createServer(app);
+
+const io = socketIo(server);
+
+const users = {};
+
+io.on("connection", (socket) => {
+	if (!users[socket.id]) {
+		users[socket.id] = socket.id;
+	}
+	socket.emit("yourID", socket.id);
+	console.log("New client connected:", socket.id);
+	socket.on("disconnect", () => {
+		console.log("Client disconnected:", socket.id);
+	});
+	socket.on("play/pause", (msg) => {
+		console.log("play/pause: ", msg);
+		io.emit("play/pause", msg);
+	});
+	socket.on("sync", (time) => {
+		console.log("sync: ", time);
+		io.emit("sync", time);
+	});
+	socket.on("seekTo", (time) => {
+		console.log("seekTo: ", time);
+		io.emit("seekTo", time);
+	});
+	socket.on("shareUrl", (url) => {
+		console.log("shareUrl: ", url);
+		io.emit("shareUrl", url);
+	});
+	socket.on("callUser", (data) => {
+		console.log("Incoming call from: ", socket.id);
+		io.emit("hey", data);
+	});
+	socket.on("acceptCall", (data) => {
+		io.emit("callAccepted", data);
+	});
+});
+
+if (process.env.PROD) {
+	app.use(express.staticc(path.join(__dirname, "./front/build")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "./front/build/index.html"));
+	});
+}
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ front/src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import "normalize.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+import "./App.scss";
+
+import socketIOClient from "socket.io-client";
+// const ENDPOINT = "http://localhost:4001";
+const ENDPOINT = "https://6d707240a653.ngrok.io/";
+
+console.log("initiating socket connection ...");
+const socket = socketIOClient(ENDPOINT);
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App socket={socket} />
+	</React.StrictMode>,
+	document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
